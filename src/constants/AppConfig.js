f45511fd04d161935cc30c42353fcc6013b3ac47
Snehal Kale <snehal.kale@eternussolutions.com>
2018-11-20 18:54:11 +0530

    changed server url to production

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -2,8 +2,7 @@
  * App Config File
  */
 const AppConfig = {
-  serverURL: "http://localhost:5000" //local URL
-  //serverURL: "http://192.168.101.21:3010"
+  // serverURL: "http://localhost:5000" //local URL
+  serverURL: "https://interceptorapi.azurewebsites.net" //production URL
 };
-
 export default AppConfig;
